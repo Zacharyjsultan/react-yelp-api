7366fc438c4f440b0d0b657a32e2b7b64cd9d7ed
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-10-11 12:40:14 -0700

    index

--- functions/fetch-yelp/index.js
+++ functions/fetch-yelp/index.js
@@ -2,6 +2,30 @@ const fetch = require('node-fetch');
 require('dotenv').config({ path: `.env.development.local` });
 
 const handler = async (event) => {
+  try {
+    const response = await fetch(
+      `https://api.yelp.com/v3/businesses/search?categories=restaurants`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
+        },
+      }
+    );
+    const data = await response.json();
+    // eslint-disable-next-line no-console
+    console.log(data);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.businesses),
+    };
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch la data?!' }),
+    };
+  }
   // add code here to fetch data from yelp API
   // be sure to include the parameters from event.queryStringParameters
 };
